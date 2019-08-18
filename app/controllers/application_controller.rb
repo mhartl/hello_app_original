8f1231e74dcf661a58b6032533f3cb493b90b65c
Michael Hartl <michael@michaelhartl.com>
2019-08-18 19:20:46 +0000

    Update message and route

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
 
   def hello
-    render html: "¡Hola, mundo!"
+    render html: "hello, world!"
   end
   
   def goodbye
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,3 @@
 Rails.application.routes.draw do
-  root 'application#goodbye'
+  root 'application#hello'
 end
